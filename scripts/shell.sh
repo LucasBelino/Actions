569361919dd8d4dbb35fa88d10d63cca50765155
lucas belino <lucas.belino@incoway.com.br>
2025-04-09 02:07:58 -0300

    Ajustando job de email

--- scripts/shell.sh
+++ scripts/shell.sh
@@ -3,4 +3,4 @@ ls
 echo "Envio de e-mail"
 pwd
 sudo apt-get install mailutils
-echo "Mandando e-mail com mail do linux" | mail -s "a subject" lucas23belino@gmail.com
+echo "Mandando e-mail com mail do linux" | mail -s "Notificação Pipeline" "$RECIPIENT_EMAIL"
